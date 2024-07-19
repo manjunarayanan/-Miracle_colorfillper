2b69ac4f001eb4e27cf46675325e20e9ddfecf29
manjunarayanan <manjunarayanan24@gmail.com>
2024-07-19 18:08:37 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+const button = document.getElementById('btn');
+const colorDisplay = document.getElementById('color');
+
+button.addEventListener('click', () => {
+    const randomColor = getRandomColor();
+    document.body.style.backgroundColor = randomColor;
+    colorDisplay.textContent = randomColor;
+});
+
+button.addEventListener('mouseover', () => {
+    button.style.backgroundColor = getRandomColor();
+});
+
+button.addEventListener('mouseout', () => {
+    button.style.backgroundColor = 'yellow';
+});
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
